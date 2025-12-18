ba94ca8e4fc5e09419bf4ad16813481b03f7b99e
Basma Elmahany <basmakhalaf974@gmail.com>
2025-12-18 13:09:49 +0200

    layout stablish

--- src/app/Layout/Components/dashboard-layout/dashboard-layout.component.ts
+++ src/app/Layout/Components/dashboard-layout/dashboard-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { I18nService } from '../../../Shared/Services/i18n.service';
 
 @Component({
   selector: 'app-dashboard-layout',
@@ -7,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard-layout.component.scss'
 })
 export class DashboardLayoutComponent {
+   isCollapsed = false;
 
+  constructor(public i18n: I18nService) {}
+
+  toggleLang(): void {
+    this.i18n.toggleLanguage();
+  }
+
+  toggleSidebar(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
